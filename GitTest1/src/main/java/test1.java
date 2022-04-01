be3fdc8b29d46fb46d716a6c3ec1df95cdd69be8
smhrd <smhrd@DESKTOP-R53LD97>
2022-04-01 12:29:45 +0900

    네번째

--- GitTest1/src/main/java/test1.java
+++ GitTest1/src/main/java/test1.java
@@ -5,12 +5,12 @@ public class test1 {
 
 		System.out.println("첫번쨰 커밋");
 		System.out.println("두번쨰 커밋");
-		System.out.println("세번째 커밋");
-		//pull, push ---> git 원격 저장소와
-		//Local repository의 통신이다.
-		// !중요! pull, push 하기 전에는 꼭
-		//Local repository를 최신화 시켜줘야한다.(이클립스코드와 같아야한다)
-		// == 커밋을 해줘야한다.
+		
+		// pull, push -> git 원격 저장소와 local repository의 통신이다
+		// * pull, push 하기 전에는 꼭 local repository를 최신화 시켜줘야한다 (이클립스 코드와 같아야한다)
+		// == 커밋을 해줘야한다
+		System.out.println("네번째 커밋");
+
 
 	}
 
