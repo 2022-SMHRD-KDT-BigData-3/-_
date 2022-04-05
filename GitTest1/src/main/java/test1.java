4132d47a9c751ad7048cd6da1fd955c3c7c8b5f7
smhrd <smhrd@DESKTOP-TGN0PVU>
2022-04-05 11:43:53 +0900

    재민 pull

--- GitTest1/src/main/java/test1.java
+++ GitTest1/src/main/java/test1.java
@@ -3,16 +3,14 @@ public class test1 {
 
 	public static void main(String[] args) {
 
-		System.out.println("첫번쨰 커밋");
-		System.out.println("두번쨰 커밋");
+		System.out.println("첫번째 커밋");
+		System.out.println("두번째 커밋");
+		System.out.println("세번째 커밋"); 
+		// pull : 받아오기 --> github원격 저장소와 Local repository의 통신이다.
+		// push : 보내기
+		// pull, push 하기 전에 꼭 Local repository를 최신화 시켜줘야한다!(이클립스 코드와 같아야 한다) == 커밋을 해줘야 한다.
+		System.out.println("다섯번째 커밋");
 
-		System.out.println("세번째 커밋");
-		//pull, push ---> git 원격 저장소와
-		//Local repository의 통신이다.
-		// !중요! pull, push 하기 전에는 꼭
-		//Local repository를 최신화 시켜줘야한다.(이클립스코드와 같아야한다)
-		// == 커밋을 해줘야한다.
-		
 	}
 
 }
